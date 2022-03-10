b4aed70b7e27d11892b25167ff9062b29a106f0c
GameGenesis <gamegenesisdev@gmail.com>
2022-03-10 18:20:24 -0500

    Update Track and Playlist classes
    * Store more information about the track or playlist
    
    * Add get_info_string method to the Playlist class to display information about the playlist in a string format

--- src/playlist.py
+++ src/playlist.py
@@ -1,5 +1,9 @@
 class Playlist:
-    def __init__(self, title, artist, date_created):
+    def __init__(self, path, title, artist, date_created):
+        self.path = path
         self.title = title
         self.artist = artist
-        self.date_added = date_created
+        self.date_created = date_created
+
+    def get_info_string(self):
+        return f"Title: {self.title}, Artist: {self.artist}, Date Created: {self.date_created}, Path: {self.path}"
--- src/track.py
+++ src/track.py
@@ -1,5 +1,6 @@
 class Track:
-    def __init__(self, title, artist, album, date_added):
+    def __init__(self, path, title, artist, album, date_added):
+        self.path = path
         self.title = title
         self.artist = artist
         self.album = album
