ca1a67b56ff48d5d1c01b3b10371a5551a3b4958
GameGenesis <gamegenesisdev@gmail.com>
2022-03-10 18:56:29 -0500

    Update Track class
    * Automatically get date added and track duration

--- src/track.py
+++ src/track.py
@@ -1,7 +1,19 @@
+from mutagen.mp3 import MP3
+from datetime import datetime
+import os
+
 class Track:
-    def __init__(self, path, title, artist, album, date_added):
+    def __init__(self, path, title, artist, album):
         self.path = path
         self.title = title
         self.artist = artist
         self.album = album
-        self.date_added = date_added
+
+    def get_date_added(self):
+        return datetime.fromtimestamp(os.path.getctime(self.path))
+
+    def get_duration(self):
+        return int(MP3(self.path).info.length)
+
+    date_created = property(fget=get_date_added, doc="The date the playlist was created.")
+    duration = property(fget=get_duration, doc="The duration of the track.")
