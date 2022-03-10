347ac9a0257c86452b236dc506e7181fe20fb0e1
GameGenesis <gamegenesisdev@gmail.com>
2022-03-10 18:41:24 -0500

    Update Playlist class
    * Store playlist tracks
    
    * Add number of tracks and total duration of all tracks methods and properties

--- src/playlist.py
+++ src/playlist.py
@@ -1,9 +1,19 @@
 class Playlist:
-    def __init__(self, path, title, artist, date_created):
+    def __init__(self, path, title, artist, date_created, tracks):
         self.path = path
         self.title = title
         self.artist = artist
         self.date_created = date_created
+        self.tracks = tracks
+
+    def get_length(self):
+        return len(self.tracks)
+
+    def get_total_duration(self):
+        return 0
 
     def get_info_string(self):
         return f"Title: {self.title}, Artist: {self.artist}, Date Created: {self.date_created}, Path: {self.path}"
+
+    length = property(fget=get_length, doc="The number of tracks in the playlist.")
+    total_duration = property(fget=get_total_duration, doc="The sum of the durations of all the tracks in the playlist.")
