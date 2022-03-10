2f3d65728ce5ff4d1fbacca95fea5f1d50130f92
GameGenesis <gamegenesisdev@gmail.com>
2022-03-10 18:54:46 -0500

    Update Playlist Class
    * Automatically calculate date created

--- src/playlist.py
+++ src/playlist.py
@@ -1,10 +1,15 @@
+from datetime import datetime
+import os
+
 class Playlist:
-    def __init__(self, path, title, artist, date_created, tracks):
+    def __init__(self, path, title, artist, tracks):
         self.path = path
         self.title = title
         self.artist = artist
-        self.date_created = date_created
         self.tracks = tracks
+
+    def get_date_created(self):
+        return datetime.fromtimestamp(os.path.getctime(self.path))
 
     def get_length(self):
         return len(self.tracks)
@@ -15,5 +20,6 @@ class Playlist:
     def get_info_string(self):
         return f"Title: {self.title}, Artist: {self.artist}, Date Created: {self.date_created}, Path: {self.path}"
 
+    date_created = property(fget=get_date_created, doc="The date the playlist was created.")
     length = property(fget=get_length, doc="The number of tracks in the playlist.")
     total_duration = property(fget=get_total_duration, doc="The sum of the durations of all the tracks in the playlist.")
