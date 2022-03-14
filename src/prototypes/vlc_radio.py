5a9aa14dd7f70759d2739e9d5b7dfb3789c1e0ef
GameGenesis <gamegenesisdev@gmail.com>
2022-03-13 23:38:20 -0400

    Add instructions for finding radio streams & add examples
    * Add instructions on how to find stream URLs
    
    * Add example radio streams for testing

---
+++ src/prototypes/vlc_radio.py
@@ -0,0 +1,19 @@
+'''
+Finding the stream URL using Chrome Dev Tools or Mozilla Firefox Firebug:
+- Right click
+- View page source
+- Find stream urls
+'''
+
+'''
+99.9 Virgin Radio
+"hls_stream":"http://playerservices.streamtheworld.com/api/livestream-redirect/CKFMFM_ADP.m3u8"
+"secure_mp3_pls_stream":"https://playerservices.streamtheworld.com/pls/CKFMFM.pls"
+"pls_stream":"http://playerservices.streamtheworld.com/pls/CKFMFMAAC.pls"
+"secure_hls_stream":"https://playerservices.streamtheworld.com/api/livestream-redirect/CKFMFM_ADP.m3u8"
+"secure_pls_stream":"https://playerservices.streamtheworld.com/pls/CKFMFMAAC.pls"
+
+Belgian radio station (MNM) example: http://icecast.vrtcdn.be/mnm-high.mp3 (includes now playing info)
+
+IHeart radio playlist: https://playerservices.streamtheworld.com/pls/ST13_S01.pls
+'''
