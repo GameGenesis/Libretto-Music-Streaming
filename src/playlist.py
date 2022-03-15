0904c147b3839e611818376c49b47ef4ea871141
GameGenesis <gamegenesisdev@gmail.com>
2022-03-15 10:58:08 -0400

    Implement playlist total duration & remove unnecessary methods
    * Implement playlist total duration
    
    * Remove unnecessary methods
    
    * Update info string format

--- src/playlist.py
+++ src/playlist.py
@@ -8,18 +8,13 @@ class Playlist:
         self.artist = artist
         self.tracks = tracks
 
-    def get_date_created(self):
-        return datetime.fromtimestamp(os.path.getctime(self.path))
+        self.date_created = datetime.fromtimestamp(int(os.path.getctime(self.path)))
+        self.length = len(self.tracks)
+        self.total_duration = self.get_total_duration()
 
-    def get_length(self):
-        return len(self.tracks)
 
     def get_total_duration(self):
-        return 0
+        return sum([t.duration for t in self.tracks])
 
     def get_info_string(self):
-        return f"Title: {self.title}, Artist: {self.artist}, Date Created: {self.date_created}, Path: {self.path}"
-
-    date_created = property(fget=get_date_created, doc="The date the playlist was created.")
-    length = property(fget=get_length, doc="The number of tracks in the playlist.")
-    total_duration = property(fget=get_total_duration, doc="The sum of the durations of all the tracks in the playlist.")
+        return f"Title: {self.title}, Artist: {self.artist}, Date Created: {self.date_created}, Total Duration: {self.total_duration}s, Path: {self.path}"
