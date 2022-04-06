31ffddde658c4d283db46ff1c328d7033b095773
Rayan K <gamegenesisdev@gmail.com>
2022-04-06 13:20:14 -0400

    Create SQLAlchemy prototype database example
    * Create and read database values
    
    * Create objects and relationships

---
+++ src/prototypes/database.py
@@ -0,0 +1,160 @@
+from datetime import date
+
+import sqlalchemy as db
+
+from sqlalchemy import Column, Integer, String, ForeignKey, Table, Date, DateTime, Boolean
+from sqlalchemy.orm import relationship, backref, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = db.create_engine('sqlite:///database.db')
+Base = declarative_base()
+
+class Actor(Base):
+    __tablename__ = 'actors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    birthday = Column(Date)
+
+    def __init__(self, name, birthday):
+        self.name = name
+        self.birthday = birthday
+
+class Stuntman(Base):
+    __tablename__ = 'stuntmen'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    active = Column(Boolean)
+    actor_id = Column(Integer, ForeignKey('actors.id'))
+    actor = relationship("Actor", backref=backref("stuntman", uselist=False))
+
+    def __init__(self, name, active, actor):
+        self.name = name
+        self.active = active
+        self.actor = actor
+
+movies_actors_association = Table(
+    'movies_actors', Base.metadata,
+    Column('movie_id', Integer, ForeignKey('movies.id')),
+    Column('actor_id', Integer, ForeignKey('actors.id'))
+)
+
+class Movie(Base):
+    __tablename__ = 'movies'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    release_date = Column(Date)
+    actors = relationship("Actor", secondary=movies_actors_association)
+
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+
+class ContactDetails(Base):
+    __tablename__ = 'contact_details'
+
+    id = Column(Integer, primary_key=True)
+    phone_number = Column(String)
+    address = Column(String)
+    actor_id = Column(Integer, ForeignKey('actors.id'))
+    actor = relationship("Actor", backref="contact_details")
+
+    def __init__(self, phone_number, address, actor):
+        self.phone_number = phone_number
+        self.address = address
+        self.actor = actor
+
+Base.metadata.create_all(engine)
+
+Session = sessionmaker()
+Session.configure(bind=engine)
+session = Session()
+
+bourne_identity = Movie("The Bourne Identity", date(2002, 10, 11))
+furious_7 = Movie("Furious 7", date(2015, 4, 2))
+pain_and_gain = Movie("Pain & Gain", date(2013, 8, 23))
+
+# 5 - creates actors
+matt_damon = Actor("Matt Damon", date(1970, 10, 8))
+dwayne_johnson = Actor("Dwayne Johnson", date(1972, 5, 2))
+mark_wahlberg = Actor("Mark Wahlberg", date(1971, 6, 5))
+
+# 6 - add actors to movies
+bourne_identity.actors = [matt_damon]
+furious_7.actors = [dwayne_johnson]
+pain_and_gain.actors = [dwayne_johnson, mark_wahlberg]
+
+# 7 - add contact details to actors
+matt_contact = ContactDetails("415 555 2671", "Burbank, CA", matt_damon)
+dwayne_contact = ContactDetails("423 555 5623", "Glendale, CA", dwayne_johnson)
+dwayne_contact_2 = ContactDetails("421 444 2323", "West Hollywood, CA", dwayne_johnson)
+mark_contact = ContactDetails("421 333 9428", "Glendale, CA", mark_wahlberg)
+
+# 8 - create stuntmen
+matt_stuntman = Stuntman("John Doe", True, matt_damon)
+dwayne_stuntman = Stuntman("John Roe", True, dwayne_johnson)
+mark_stuntman = Stuntman("Richard Roe", True, mark_wahlberg)
+
+# 9 - persists data
+session.add(bourne_identity)
+session.add(furious_7)
+session.add(pain_and_gain)
+
+session.add(matt_contact)
+session.add(dwayne_contact)
+session.add(dwayne_contact_2)
+session.add(mark_contact)
+
+session.add(matt_stuntman)
+session.add(dwayne_stuntman)
+session.add(mark_stuntman)
+
+# 10 - commit and close session
+session.commit()
+session.close()
+
+# 2 - extract a session
+session = Session()
+
+# 3 - extract all movies
+movies = session.query(Movie).all()
+
+# 4 - print movies' details
+print('\n### All movies:')
+for movie in movies:
+    print(f'{movie.title} was released on {movie.release_date}')
+print('')
+
+# 5 - get movies after 15-01-01
+movies = session.query(Movie) \
+    .filter(Movie.release_date > date(2015, 1, 1)) \
+    .all()
+
+print('### Recent movies:')
+for movie in movies:
+    print(f'{movie.title} was released after 2015')
+print('')
+
+# 6 - movies that Dwayne Johnson participated
+the_rock_movies = session.query(Movie) \
+    .join(Actor, Movie.actors) \
+    .filter(Actor.name == 'Dwayne Johnson') \
+    .all()
+
+print('### Dwayne Johnson movies:')
+for movie in the_rock_movies:
+    print(f'The Rock starred in {movie.title}')
+print('')
+
+# 7 - get actors that have house in Glendale
+glendale_stars = session.query(Actor) \
+    .join(ContactDetails) \
+    .filter(ContactDetails.address.ilike('%glendale%')) \
+    .all()
+
+print('### Actors that live in Glendale:')
+for actor in glendale_stars:
+    print(f'{actor.name} has a house in Glendale')
+print('')
