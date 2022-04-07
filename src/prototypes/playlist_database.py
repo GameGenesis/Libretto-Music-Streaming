5a4d2c349b65c2e70d6b95c3daea08156929e7b5
Rayan K <gamegenesisdev@gmail.com>
2022-04-07 13:25:31 -0400

    Update playlist database
    * Create stream table
    
    * Modify table id names

--- src/prototypes/playlist_database.py
+++ src/prototypes/playlist_database.py
@@ -10,13 +10,13 @@ Base = declarative_base()
 playlist_track = Table(
     "playlist_track",
     Base.metadata,
-    Column("playlist_id", Integer, ForeignKey("playlist.playlist_id")),
-    Column("track_id", Integer, ForeignKey("track.track_id")),
+    Column("playlist_id", Integer, ForeignKey("playlist.id")),
+    Column("track_id", Integer, ForeignKey("track.id")),
 )
 
 class Playlist(Base):
     __tablename__ = "author"
-    playlist_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     title = Column(String)
     artist = Column(String)
     date_created = Column(DateTime)
@@ -26,8 +26,17 @@ class Playlist(Base):
 
 class Track(Base):
     __tablename__ = "track"
-    track_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     title = Column(String)
     playlists = relationship(
         "Playlists", secondary=playlist_track, back_populates="tracks"
     )
+
+class Stream(Base):
+    __tablename__ = 'stream'
+
+    id = Column(Integer, primary_key=True)
+    url = Column(String)
+
+    def __init__(self, url):
+        self.url = url
