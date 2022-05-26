0b2c3036f414190d8b4ad799dcb313ee8f29cd51
GameGenesis <gamegenesisdev@gmail.com>
2022-05-26 16:22:56 -0400

    Update YouTube search prototype
    * Display more info about the search and convert to StreamData object

--- src/prototypes/yt_search.py
+++ src/prototypes/yt_search.py
@@ -1,7 +1,25 @@
+import inspect
+import os
+import sys
 from youtubesearchpython import VideosSearch
 
-videosSearch = VideosSearch('Circles - Official Audio', limit = 10)
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
+from stream import StreamData
+
+videosSearch = VideosSearch('Lea Makhoul Ratata', limit = 1)
 results = videosSearch.result()["result"]
 
 for result in results:
+    print(result["link"])
     print(result["title"])
+    print(result["channel"]["name"])
+    print(result["duration"])
+    print(result["publishedTime"])
+    print(result["thumbnails"][0]["url"])
+    print(result["thumbnails"][1]["url"])
+    print()
+    stream_data = StreamData(result["link"])
+    print(stream_data.title, stream_data.artist, stream_data.album, stream_data.duration)
