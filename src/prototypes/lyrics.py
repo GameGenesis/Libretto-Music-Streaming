d3156350f84262b6e69e7421f099b2c6a4996a3c
GameGenesis <gamegenesisdev@gmail.com>
2022-05-29 22:29:24 -0400

    Update Genius lyrics prototype
    * Update regex for song lyrics
    
    * Add genre search prototype code
    
    * Add general search test

--- src/prototypes/lyrics.py
+++ src/prototypes/lyrics.py
@@ -19,13 +19,29 @@ genius = lg.Genius(config.GENIUS_ACCESS_TOKEN, skip_non_songs=True, excluded_ter
 song = genius.search_song(title="Circles", artist="Post Malone")
 
 print(song.artist, song.title, song.url, song.full_title, song.header_image_thumbnail_url, song.header_image_url, song.song_art_image_thumbnail_url, song.song_art_image_url, sep="\n")
-print(re.sub(r"\B\d+Embed", "", song.lyrics))
+# Replace html artifact in lyrics (Remove number followed by "Embed" if it comes right after any character that isn't a number)
+print(re.sub(r"([^0-9])?\d+Embed", r"\1", song.lyrics))
+# song.save_lyrics('lyrics.json')
 
-song.save_lyrics('lyrics.json')
+print()
+
+# Top 100 song results in the Pop genre
+tag_results = list()
+for i in range(5):
+    for result in genius.tag("pop", page=i).get("hits"):
+        tag_results.append(result)
+
+for result in tag_results:
+    print(result.get("title"))
+
+print()
+
+songs = genius.search("Post Malone")
+print(songs)
 
 # Search for a song in the search bar; gives the top results with Genius (with a button to load more results, and specify number of results)
 # Also gives results with YouTube search (for podcasts, etc.)
-# When downloading, downloads and configures metadata and cover art and gets info from downloaded Genius json
+# When downloading, downloads and configures metadata and cover art and gets info from Genius
 
 #Searches all of "Pop Smoke"'s songs.
 #artist = genius.search_artist("Pop Smoke")
